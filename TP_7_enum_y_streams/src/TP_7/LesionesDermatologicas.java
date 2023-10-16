dac3cc6e6441a2121b889f66c4323b2e66d50ff6
IgnacioLattenero <ignaciolattenero@gmail.com>
2023-10-16 15:26:56 -0300

    escribo enum LesionesDermatologicas, pero queda hacerlo rotativo

--- TP_7_enum_y_streams/src/TP_7/LesionesDermatologicas.java
+++ TP_7_enum_y_streams/src/TP_7/LesionesDermatologicas.java
@@ -1,8 +1,28 @@
 package TP_7;
 
 public enum LesionesDermatologicas {
-	ROJO,
-	GRIS,
-	AMARILLO,
-	MIEL
+	ROJO("grave", 4),
+	GRIS("medio", 3),
+	AMARILLO("leve", 2),
+	MIEL("curado", 1);
+	
+	private final String descripcion;
+	private final int nivelDeRiesgo;
+	
+	private LesionesDermatologicas(String descripcion, int nivelDeRiesgo) {
+		this.descripcion = descripcion;
+		this.nivelDeRiesgo = nivelDeRiesgo;
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+	public int getNivelDeRiesgo() {
+		return nivelDeRiesgo;
+	}
+ 	
+	
+	
 }
+
